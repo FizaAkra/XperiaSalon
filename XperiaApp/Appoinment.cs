e3aa6869f715f0d5c055de12c262f0a709a49c12
FizaAkra <108400685+FizaAkra@users.noreply.github.com>
2022-09-14 23:05:43 -0700

    Add files via upload

---
+++ XperiaApp/Appoinment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XperiaApp.Models
+{
+   public class Appoinment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AppointmentID { get; set; }
+        public DateTime Appointment_Date { get; set; }
+        public string Appointment_Status { get; set; }
+    }
+}
